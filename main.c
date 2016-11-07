863aa482eea6d806accc5b61ccc6612cfe319ce2
MonsieurMo <charles.homo@viacesi.fr>
2016-11-07 10:00:00 +0100

    Remove addition on substraction file

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int addition(int a, int b)
-{
-    return a+b;
-}
+
 
 int soustraction(int a, int b)
 {
@@ -13,7 +10,6 @@ int soustraction(int a, int b)
 
 int main()
 {
-    printf("L'addition de a et b vaut %d\n", addition(1,4))
     printf("La soustraction de a et b vaut %d\n" soustraction(1,4)),
     return 0;
 }
